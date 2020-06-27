e1a376b2de8af9d9175120cb4fc99b4b645f7590
JesseCai12 <caichao826010@gmail.com>
2020-06-27 01:04:30 -0500

    deployment

---
+++ config/keys-prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+    MongoURL:'mongodb+srv://socialMediaAppChao:socialMediaAppChao@socialmediaapp-weyxo.mongodb.net/<dbname>?retryWrites=true&w=majority',
+    GoogleClientID: '793097372631-bmoe7juqenvcv4j81khgtuinh43fh8vh.apps.googleusercontent.com',
+    GoogleClientSecret: '9QnfiDQPfjeVjGmaA_VgoWmf'
+}
+}
