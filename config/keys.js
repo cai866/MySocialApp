c32d924a232aeba2aa8d5538377e1b8bc913e711
JesseCai12 <caichao826010@gmail.com>
2020-06-27 13:00:29 -0500

    UX design

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+const { model } = require("../models/user")
+const express = require("express")
+
+
+if(process.env.NODE_ENV === 'production') {
+    module.exports = require('./keys-prod')
+}else{
+    module.exports = require('./keys-dev')
+}
